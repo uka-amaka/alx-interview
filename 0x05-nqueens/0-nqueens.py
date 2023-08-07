b8f734bdac727bc16aded5677753a9c63d9ee408
uka-amaka <ukamaka.anthony01@gmail.com>
2023-08-07 19:40:30 +0000

    i hope i pass the requirements this time

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,23 +5,22 @@ import sys
 def is_safe(board, row, col, N):
     # Check if a queen can be placed in the given cell without attacking others
     for i in range(row):
-        if board[i] == col or \
-           abs(board[i] - col) == abs(i - row):
+        if board[i] == col or abs(board[i] - col) == abs(i - row):
             return False
     return True
 
 def print_solution(board, N):
     # Print the solution in the specified format
     for i in range(N):
-        line = ["Q" if j == board[i] else "." for j in range(N)]
-        print(" ".join(line))
+        for j in range(N):
+            if j == board[i]:
+                print("Q", end=" ")
+            else:
+                print("0", end=" ")
+        print()
     print()
 
 def solve_nqueens(N):
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
     board = [-1] * N
     solutions = []
 
@@ -33,27 +32,30 @@ def solve_nqueens(N):
                 if is_safe(board, row, col, N):
                     board[row] = col
                     place_queens(row + 1)
-                    board[row] = -1
 
     place_queens(0)
     return solutions
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+        print("Usage: ./0-nqueens.py N")
         sys.exit(1)
 
     try:
         N = int(sys.argv[1])
     except ValueError:
-        print("N must be a number")
+        print("N must be a valid integer.")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4.")
         sys.exit(1)
 
     solutions = solve_nqueens(N)
 
     if not solutions:
-        print(f"No solution found for N = {N}")
-        sys.exit(1)
-
-    for solution in solutions:
-        print_solution(solution, N)
+        print("No solution found for N = {0}".format(N))
+    else:
+        for idx, solution in enumerate(solutions, 1):
+            print("Solution {0}:".format(idx))
+            print_solution(solution, N)
